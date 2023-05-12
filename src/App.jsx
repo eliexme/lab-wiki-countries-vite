6ccefea5ac567fb864314e49d3b35ed2240ed2d5
Elisa Exposito <eliexme@gmail.com>
2023-05-12 14:42:31 +0100

    missing bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
 // src/App.jsx
 import { useEffect, useState } from 'react'
 import './App.css'
-import countries from '/src/countries.json'
+/* import countries from '/src/countries.json' */
 import Navbar from './components/Navbar'
 import CountriesList from './components/CountriesList'
 import { Route, Routes } from 'react-router-dom'
 import CountryDetails from './components/CountryDetails'
-import { useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 function App() {
   const [countriesData, setCountriesData] = useState([])
 
+  const axiosData = async()=>{
+    try {
+      const response = await axios.get("https://ih-countries-api.herokuapp.com/countries")
+      console.log(response)
+      if(response.status === 200){
+        setCountriesData(response.data)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(()=>{
-
-       fetch("https://ih-countries-api.herokuapp.com/countries")
-    .then((response) => { 
-      return response.json();
-    })
-    .then((data) => {
-      setCountriesData(data)
-    })
-    .catch( (err) => console.log(err));
-
+    axiosData()
   }, [])
+
+/*   const fetchData = async() =>{
+    try {
+      const response = await fetch("https://ih-countries-api.herokuapp.com/countries")
+      const data = await response.json()
+      const dataSorted = data.sort((a, b)=>{
+        if(a.name.common < b.name.common)return -1
+        else if(a.name.common > b.name.common)return 1
+        else return 0
+      })
+      setCountriesData(dataSorted)
+
+    } catch (error) {
+      console.log(error)
+    }
+  } */
+
+/*   useEffect(()=>{
+    fetchData()
+  }, []) */
+
+
+/*   useEffect(() => {
+    fetch("https://ih-countries-api.herokuapp.com/countries").then((res) => {
+      return res.json().then((data) => {
+        data.sort((a, b) => {
+          if (a.name.common < b.name.common) {
+            return -1;
+          } else if (a.name.common > b.name.common) {
+            return 1;
+          } else return 0;
+        });
+        setCountriesData(data);
+      });
+    });
+  }, []); */
 
   return (
   <div className='App'>
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -29,8 +29,6 @@ export default function CountryDetails({countriesData}) {
             )
         })}
       </ul>
-
-      
     </div>
   )
 }
